1c1fc19433d029d1e8598c702704e01ddf0a559f
JeremyMcCormick <jermccormick@gmail.com>
2018-08-01 21:58:35 -0700

    Updates for Geant4 10.3 compatibility.

--- src/RunManager.cxx
+++ src/RunManager.cxx
@@ -2,14 +2,23 @@
 
 #include "UnknownDecayPhysics.h"
 
+#include "G4StateManager.hh"
 #include "FTFP_BERT.hh"
 
 namespace hpssim {
 
 void RunManager::InitializePhysics() {
-    G4VUserPhysicsList* thePhysicsList = new FTFP_BERT;
-    G4VModularPhysicsList* modularPhysicsList = dynamic_cast<G4VModularPhysicsList*>(thePhysicsList);
-    modularPhysicsList->RegisterPhysics(new UnknownDecayPhysics);
+
+    // For now, do not do anything here until "state" issues are sorted out.
+
+    //G4StateManager* pStateManager = G4StateManager::GetStateManager();
+    //G4cout << "state: " << pStateManager->GetStateString(pStateManager->GetCurrentState());
+    //pStateManager->SetNewState(G4State_PreInit);
+
+    //G4VUserPhysicsList* thePhysicsList = new FTFP_BERT;
+    //G4VModularPhysicsList* modularPhysicsList = dynamic_cast<G4VModularPhysicsList*>(physicsList);
+    //modularPhysicsList->RegisterPhysics(new UnknownDecayPhysics);
+
     G4RunManager::InitializePhysics();
 }
 
