c83f42b3e87a61b7171a5b7b8eed2983557e257f
Jeremy McCormick <jermccormick@gmail.com>
2020-01-13 20:31:06 -0600

    Implement dep build

---
+++ cmake/InstallHEPPDT.cmake
@@ -0,0 +1,17 @@
+EXTERNALPROJECT_ADD(
+    HEPPDT
+
+    URL "http://lcgapp.cern.ch/project/simu/HepPDT/download/HepPDT-3.04.01.tar.gz"
+
+    SOURCE_DIR "${CMAKE_BINARY_DIR}/heppdt"
+    
+    UPDATE_COMMAND ""
+    PATCH_COMMAND ""
+    
+    CONFIGURE_COMMAND "${CMAKE_BINARY_DIR}/heppdt/configure" --prefix=${DEPENDENCY_INSTALL_DIR}/heppdt
+)
+
+if( NOT "${HEPPDT_DIR}" )
+    set( HEPPDT_DIR ${DEPENDENCY_INSTALL_DIR}/heppdt CACHE PATH "HEPPDT install dir" FORCE )
+    message( "HEPPDT_DIR=${HEPPDT_DIR}" )
+endif()
