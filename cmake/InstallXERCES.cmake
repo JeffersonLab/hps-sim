6c64f94e6567c964886a7fba7611d7ffd3c9f12b
Jeremy McCormick <jermccormick@gmail.com>
2022-01-29 13:11:58 -0600

    Update Xerces version

--- cmake/InstallXERCES.cmake
+++ cmake/InstallXERCES.cmake
@@ -1,6 +1,6 @@
 EXTERNALPROJECT_ADD(
     XERCES 
-    URL "https://downloads.apache.org/xerces/c/3/sources/xerces-c-3.2.2.tar.gz"
+    URL "https://downloads.apache.org/xerces/c/3/sources/xerces-c-3.2.3.tar.gz"
     SOURCE_DIR "${CMAKE_BINARY_DIR}/xerces"
     
     UPDATE_COMMAND ""
