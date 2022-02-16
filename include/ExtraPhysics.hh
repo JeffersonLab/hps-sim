6af4bd1ecd986b5c744cd03a09b42932ab4f28f8
Jeremy McCormick <jeremym@slac.stanford.edu>
2022-02-16 17:06:45 -0600

    Remove unneeded class

--- include/ExtraPhysics.hh
+++ include/ExtraPhysics.hh
@@ -17,60 +17,6 @@ namespace hpssim {
 #endif
 
 /**
- * @brief Wrapper for unknown decay for HPS-specific PDGIDs
- * 
- */
-class ExtraDecay: public G4UnknownDecay {
-    public:
-
-        /**
-         * Class constructor.
-         */
-        ExtraDecay() :
-                G4UnknownDecay("ExtraDecay") {
-            // DEBUG
-            SetVerboseLevel(2);
-        }
-
-        G4double GetMeanFreePath(const G4Track& aTrack, G4double, G4ForceCondition* condition) {
-            std::cout << "[ ExtraDecay ] GetMeanFreePath for " << aTrack.GetTrackID() << std::endl; 
-            *condition = StronglyForced;
-            return 0;
-        }
-
-        /**
-         * Determine if the extended decay process is applicable to the given particle.
-         * @param[in] pdef The particle definition.
-         */
-        G4bool IsApplicable(const G4ParticleDefinition &pdef) {
-            if (pdef.GetParticleType() == "extra") {
-                std::cout << "[ ExtraDecay ] Process is applicable to " << pdef.GetPDGEncoding() << std::endl;
-            } else {
-                std::cout << "[ ExtraDecay ] Process NOT applicable to " << pdef.GetPDGEncoding() << std::endl;
-            }
-            return (pdef.GetParticleType() == "extra");
-        }
-        
-        G4VParticleChange* DecayIt(const G4Track &aTrack, const G4Step &aStep) {
-            std::cout << "[ ExtraDecay ] DecayIt for track " << aTrack.GetTrackID() << std::endl;
-            const G4DecayProducts* o_products = (aTrack.GetDynamicParticle()->GetPreAssignedDecayProducts());
-            bool isPreAssigned = (o_products != 0);
-            if (isPreAssigned) {
-                std::cout << "    Pre-assigned decay products: " << o_products->entries() << std::endl; 
-            } else {
-                std::cout << "    No pre-assigned decay products" << std::endl;
-            }
-            G4UnknownDecay::DecayIt(aTrack, aStep);
-        }
-
-        
-        virtual G4VParticleChange* AtRestDoIt(const G4Track& track, const G4Step& stepData) {
-            std::cout << "[ ExtraDecay ] AtRestDoIt is forcing decay for " << track.GetTrackID() << std::endl;
-            this->DecayIt(track, stepData);
-        }
-};
-
-/**
  * @brief Add HPS-specific particles and processes
  * 
  */
@@ -89,10 +35,6 @@ class ExtraPhysics : public G4VPhysicsConstructor {
 
         void ConstructParticle() {
           
-            std::cout << "[ ExtraPhysics ] ConstructParticle" << std::endl;
-
-            //G4UnknownParticle::Definition();
-
             new G4ParticleDefinition("hps_622", 
                     100*GeV, // mass
                     0, // width
@@ -134,31 +76,16 @@ class ExtraPhysics : public G4VPhysicsConstructor {
                     );
         }       
         
-        void ConstructProcess() {
-            
-            std::cout << "[ ExtraPhysics ] ConstructProcess" << std::endl;
-
-            // Unknown decay
-            /*
-            G4ParticleTable* tbl = G4ParticleTable::GetParticleTable();
-            G4ParticleDefinition* def = tbl->FindParticle("unknown");
-            if (def) {
-                G4ProcessManager* procMgr = def->GetProcessManager();
-                procMgr->AddProcess(&unknownDecay_);
-                procMgr->SetProcessOrdering(&unknownDecay_, idxPostStep);
-            }
-            */
-
+        void ConstructProcess() {            
             // Extra decay for HPS-specific PDGIDs
             aParticleIterator->reset();
             while( (*aParticleIterator)() ){
                 G4ParticleDefinition* particle = aParticleIterator->value();
                 G4ProcessManager* pmanager = particle->GetProcessManager();
                 if (particle->GetParticleType() == "extra") {
-                    std::cout << "[ ExtraPhysics ] Adding processes for " << particle->GetPDGEncoding() << std::endl;
-                    pmanager->AddProcess(&msProcess_, -1, 1, 1); // multiple scattering
-                    //pmanager->AddProcess(&decayProcess_, -1, -1, 2);
-                    pmanager->AddProcess(&decayProcess_, -1, -1, 2);
+                    std::cout << "[ ExtraPhysics ] Adding processes for extra particle " << particle->GetPDGEncoding() << std::endl;
+                    pmanager->AddProcess(&msProcess_, -1, 1, 1); // multiple scattering to placate Geant4 tracking
+                    pmanager->AddProcess(&decayProcess_, -1, -1, 2); // decay to trigger pre-assigned decays
                 }
             }
 
@@ -166,8 +93,6 @@ class ExtraPhysics : public G4VPhysicsConstructor {
 
     protected:
         G4Decay decayProcess_;
-        //G4UnknownDecay unknownDecay_;
-        ExtraDecay extraDecayProcess_;
         G4hMultipleScattering msProcess_;       
 };
 
