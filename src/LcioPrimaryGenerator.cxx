1f6a4cb45a0bd8770d542d324090c1618adadb43
Jeremy McCormick <jeremym@slac.stanford.edu>
2022-03-08 13:03:51 -0600

    Remove debug print outs

--- src/LcioPrimaryGenerator.cxx
+++ src/LcioPrimaryGenerator.cxx
@@ -15,10 +15,10 @@ LcioPrimaryGenerator::~LcioPrimaryGenerator() {
 void LcioPrimaryGenerator::generateEvent(EVENT::LCCollection* particleColl, G4Event*anEvent) {
     std::map<EVENT::MCParticle*, G4PrimaryParticle*> particleMap;
     if (particleColl->getNumberOfElements()) {
-        std::cout << "[ LcioPrimaryGenerator ] Generating event" << std::endl;
+        //std::cout << "[ LcioPrimaryGenerator ] Generating event" << std::endl;
         for (int i = 0; i < particleColl->getNumberOfElements(); ++i) {
             auto particle = static_cast<EVENT::MCParticle*>(particleColl->getElementAt(i));
-            std::cout << "Particle #" << i << " with PID " << particle->getPDG() << std::endl;
+            //std::cout << "Particle #" << i << " with PID " << particle->getPDG() << std::endl;
             //if (particle->getGeneratorStatus() || particle->getParents().size() == 0) {
             int pid = particle->getPDG();
             double energy = particle->getEnergy() * GeV;
@@ -29,7 +29,7 @@ void LcioPrimaryGenerator::generateEvent(EVENT::LCCollection* particleColl, G4Ev
             primaryParticle->Set4Momentum(p[0] * GeV, p[1] * GeV, p[2] * GeV, energy);
             G4PrimaryVertex* vertex = nullptr;
             if (!particle->getParents().size()) {
-                std::cout << "    Adding prim vtx" << std::endl;
+                //std::cout << "    Adding prim vtx" << std::endl;
                 vertex = new G4PrimaryVertex();
                 auto origin = particle->getVertex();
                 vertex->SetPosition(origin[0] * mm, origin[1] * mm, origin[2] * mm);
@@ -42,11 +42,11 @@ void LcioPrimaryGenerator::generateEvent(EVENT::LCCollection* particleColl, G4Ev
                 }
                 G4PrimaryParticle* primaryParent = particleMap[mcpParent];
                 if (primaryParent) {
-                    std::cout << "    Adding dau" << std::endl;
+                    //std::cout << "    Adding dau" << std::endl;
                     primaryParent->SetDaughter(primaryParticle);
                     double properTime = fabs((particle->getTime() - mcpParent->getTime()) * mcpParent->getMass())
                             / mcpParent->getEnergy();
-                    std::cout << "    Proper time " << properTime << std::endl;
+                    //std::cout << "    Proper time " << properTime << std::endl;
                     primaryParent->SetProperTime(properTime * ns);
                 } else {
                     G4Exception("", "", FatalException, "Failed to find primary particle parent.");
